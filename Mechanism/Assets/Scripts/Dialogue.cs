e86ecbbe91e3d52497fa90d3c27ae8b2ac853b0c
ironpenguin222 <148375713+ironpenguin222@users.noreply.github.com>
2025-09-08 23:27:26 -0400

    Basic text progressor

---
+++ Mechanism/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "DialogueEvent")]
+public class Dialogue : ScriptableObject
+{
+    public string charName;
+    [TextArea] public string dialogueText;
+    public List<Choices> choices;
+}
+
+[System.Serializable]
+public class Choices
+{
+    [TextArea] public string optionText;
+    public bool givesAffection;
+    public int affectionGiven;
+}
